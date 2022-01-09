437e91d9580f536eb9488264fe631b302ab56449
Farooq <farooq1.khan@ryerson.ca>
2022-01-09 12:13:20 -0500

    setup method created

--- tests.py
+++ tests.py
@@ -1,5 +1,28 @@
-from xUnit import WasRun
+from xUnit import WasRun, TestCase
+'''
 test = WasRun("testMethod")
 print(test.wasRun)
 test.run()
 print(test.wasRun)
+'''
+class TestCaseTest(TestCase):
+    def setUp(self):
+        self.test = WasRun("testMethod")
+
+
+    def testRunning(self):
+        #test = WasRun("testMethod")
+        #assert (not test.wasRun)
+        self.test.run()
+        assert self.test.wasRun
+
+    
+    def testSetUp(self):
+        #test = WasRun("testMethod")
+        self.test.run()
+        assert self.test.wasSetUp
+
+TestCaseTest("testRunning").run()
+TestCaseTest("testSetUp").run()
+
+
--- xUnit.py
+++ xUnit.py
@@ -3,16 +3,26 @@ class TestCase:
     def __init__(self, name):
         self.name = name
 
+    def run(self):
+        self.setUp()
+        method = getattr(self, self.name)
+        method()
+
+    def setUp(self):
+        pass
+
+
 class WasRun(TestCase):
     def __init__(self, name):
         self.wasRun = None # should'nt it be False initinally
+        self.wasSetUp = 1
         TestCase.__init__(self, name) 
 
     def testMethod(self):
         self.wasRun = 1
 
-    def run(self):
-        method = getattr(self, self.name)
-        method()#self.testMethod()
+    def setUp(self):
+        self.wasRun = None
+        self.wasSetUp = 1
 
 
