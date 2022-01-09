84f2a57c9accef3bc026bfad24fddc9dda96ef37
Farooq <farooq1.khan@ryerson.ca>
2022-01-09 11:58:19 -0500

    Note take a concrete example and generalize rather than generalizing from reason alone

--- tests.py
+++ tests.py
@@ -1,5 +1,5 @@
 from xUnit import WasRun
 test = WasRun("testMethod")
 print(test.wasRun)
-test.testMethod()
+test.run()
 print(test.wasRun)
--- xUnit.py
+++ xUnit.py
@@ -1,8 +1,18 @@
-class WasRun:
+class TestCase: 
+    
+    def __init__(self, name):
+        self.name = name
+
+class WasRun(TestCase):
     def __init__(self, name):
         self.wasRun = None # should'nt it be False initinally
+        TestCase.__init__(self, name) 
 
     def testMethod(self):
         self.wasRun = 1
+
+    def run(self):
+        method = getattr(self, self.name)
+        method()#self.testMethod()
 
 
