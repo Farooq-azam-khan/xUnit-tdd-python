8dd7a19887b91cc36096aed719200bae2410bf6b
Farooq <farooq1.khan@ryerson.ca>
2022-01-09 12:21:50 -0500

    simplify

--- tests.py
+++ tests.py
@@ -1,19 +1,12 @@
 from xUnit import WasRun, TestCase
 
 class TestCaseTest(TestCase):
-    def setUp(self):
-        self.test = WasRun("testMethod")
 
     def testTemplateMethod(self):
+        self.test = WasRun("testMethod")
         self.test.run()
         assert self.test.wasRun
 
-    
-    def testSetUp(self):
-        self.test.run()
-        assert "setUp testMethod " == self.test.log
-
 TestCaseTest("testTemplateMethod").run()
-TestCaseTest("testSetUp").run()
 
 
